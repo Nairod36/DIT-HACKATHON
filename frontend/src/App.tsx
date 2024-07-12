be68abc32420357366cc20bd2378a26a604d3a2b
Nairod36 <moulin.solal@gmail.com>
2024-07-12 22:10:06 +0200

    init route & start pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// src/components/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h2>Accueil</h2>
+      <p>Bienvenue sur la page d'accueil !</p>
+    </div>
+  );
+};
+
+export default Home;
