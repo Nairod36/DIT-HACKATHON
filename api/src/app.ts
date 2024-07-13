dac1b72a050ad3718b962728b983183e0bdad8b4
MtThoas <matthias.pecquery@gmail.com>
2024-07-13 23:04:04 +0200

    feat: fileUpload to img middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import verificationRoutes from './routes/verification.route';
-import participationRoutes from './routes/participation.route';
-import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+import express from "express";
+import { BaseEntity, DataSource } from "typeorm";
 import { Participation } from "./entities/participation.entity";
-import dotenv from 'dotenv';
+import { Product } from "./entities/product.entity";
+import participationRoutes from "./routes/participation.route";
+import verificationRoutes from "./routes/verification.route";
 
 dotenv.config();
 
@@ -12,32 +13,33 @@ const PORT = process.env.POSTGRES_PORT;
 
 // Configuration de l'objet DataSource en utilisant process.env
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(PORT!) || 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    entities: [Participation],
-    synchronize: true,
-    logging: false,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(PORT!) || 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: [Participation, Product, BaseEntity],
+  synchronize: true,
+  logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // Initialisation de la connexion à la base de données et démarrage du serveur
 AppDataSource.initialize()
-.then(() => {
-console.log("Connecté à la base de données avec succès");
+  .then(() => {
+    console.log("Connecté à la base de données avec succès");
 
     app.use(express.json());
-    app.use('/api', verificationRoutes());
-    app.use('/api', participationRoutes(AppDataSource));
+    app.use("/api", verificationRoutes());
+    app.use("/api", participationRoutes(AppDataSource));
 
     app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+      console.log("Server is running on port 3000");
     });
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.error("Erreur lors de la connexion à la base de données", error);
-});
+  });
