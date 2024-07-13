66858b5568ff1ea928337d5f0c7c69cbf296078e
MtThoas <matthias.pecquery@gmail.com>
2024-07-13 13:31:11 +0200

    feat: market list & product details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Header } from "./components/header";
+import { Header } from "./components/common/Header";
 import { Home } from "./pages/Home";
 import UploadFile from "./pages/UploadFile";
+import Market from "./pages/Market";
+import NFTDetails from "./pages/NFTDetails";
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    <Router>
-      <div className="App h-screen w-screen">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/upload" element={<UploadFile />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+      <Router>
+        <div className="App h-screen w-screen">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/upload" element={<UploadFile />} />
+              <Route path="/product/:id" element={<NFTDetails />} />
+              <Route path="/market" element={<Market />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </QueryClientProvider>
   );
 };
