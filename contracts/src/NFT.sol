8b1b3324d28d1125f6ac441ed05269740c588f0d
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-07-14 04:25:34 +0200

    add NFT contract

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -2,8 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ReentrancyGuard {
+contract NFT is ReentrancyGuard, Ownable, ERC721URIStorage {
+    uint256 private _nftId;
+    mapping(uint256 => address) nftToOwner;
+    mapping(address => uint256) ownerNftCount;
+    mapping(address => uint256[]) userNfts;
+    mapping(uint256 => Auction) auctions;
+
+    uint256[] public nftsInAuction;
+    Auction[] public nftsAuctionEnded;
+
+    struct Auction {
+        uint256 id;
+        bool ended;
+        address bestAuctionAddress;
+        uint256 bestAuctionAmount;
+        uint256 auctionEnding;
+        address[] auctioners;
+        uint256[] amounts;
+    }
+
+    constructor() ERC721("Caramel", "CRML") Ownable(msg.sender) {
+        _nftId = 0;
+    }
+
     // Mapping to store user addresses
     mapping(address => bool) private storedAddresses;
 
@@ -25,5 +51,181 @@ contract NFT is ReentrancyGuard {
     // Function to check if an address is stored
     function isAddressStored(address userAddress) external view returns (bool) {
         return storedAddresses[userAddress];
+    }
+
+    function mintNFT(
+        address _recipient,
+        string memory _tokenURI
+    ) external onlyOwner returns (uint256) {
+        _mint(_recipient, _nftId);
+        _setTokenURI(_nftId, _tokenURI);
+        uint256 id = _nftId;
+        _nftId++;
+        nftToOwner[id] = _recipient;
+        ownerNftCount[_recipient]+=1;
+        userNfts[_recipient].push(id);
+        return id;
+    }
+
+    function updateNFT(
+        uint256 _id,
+        string memory _tokenURI
+    ) external {
+        _setTokenURI(_id, _tokenURI);
+    }
+
+    function startAuction(
+        uint256 _id,
+        uint256 _price,
+        address _seller
+    ) external {
+        require(
+            nftToOwner[_id] == _seller,
+            "You are not the owner of this NFT"
+        );
+        require(
+            _indexOf(_id) == nftsInAuction.length,
+            "This NFT is already in an auction"
+        );
+
+        auctions[_id] = Auction({
+            id: _id,
+            ended: false,
+            bestAuctionAddress: _seller,
+            bestAuctionAmount: _price,
+            auctionEnding: block.timestamp + 1 days,
+            auctioners: new address[](0),
+            amounts: new uint256[](0)
+        });
+
+        nftsInAuction.push(_id);
+        uint256 index = _indexInNfts(_id, _seller);
+        uint256[] storage userNftArray = userNfts[_seller];
+        userNftArray[index] = userNftArray[userNftArray.length - 1];
+        userNftArray.pop();
+    }
+
+    function _indexOf(uint256 _id) internal view returns (uint256) {
+        for (uint256 index = 0; index < nftsInAuction.length; index++) {
+            if (nftsInAuction[index] == _id) return index;
+        }
+        return nftsInAuction.length;
+    }
+
+    function _indexInNfts(
+        uint256 _id,
+        address _owner
+    ) internal view returns (uint256) {
+        for (uint256 index = 0; index < userNfts[_owner].length; index++) {
+            if (userNfts[_owner][index] == _id) return index;
+        }
+        return userNfts[_owner].length;
+    }
+
+    function _getBet(
+        Auction memory _auction,
+        address _auctioner
+    ) internal pure returns (uint256) {
+        for (uint256 i = 0; i < _auction.auctioners.length; i++) {
+            if (_auctioner == _auction.auctioners[i]) return i;
+        }
+        return _auction.auctioners.length;
+    }
+
+    function makeOffer(uint256 _id, uint256 _price) payable external {
+        Auction storage auction = auctions[_id];
+        uint256 currentPrice = 0;
+        uint256 index = _getBet(auction, msg.sender);
+        if (index != auction.auctioners.length) {
+            currentPrice = auction.amounts[index];
+        }
+
+        require(block.timestamp <= auction.auctionEnding, "Auction has ended.");
+        require(
+            _price + currentPrice > auction.bestAuctionAmount,
+            "There is already a higher bid."
+        );
+
+        // TODO TRANSFER
+
+        if (index != auction.auctioners.length) {
+            auction.amounts[index] += _price;
+        } else {
+            auction.auctioners.push(msg.sender);
+            auction.amounts.push(_price);
+        }
+
+        // Update the auction
+        auction.bestAuctionAddress = msg.sender;
+        auction.bestAuctionAmount = _price + currentPrice;
+    }
+
+    function endAuction(uint256 _id) internal {
+        Auction storage auction = auctions[_id];
+
+        require(block.timestamp >= auction.auctionEnding, "Auction not ended");
+        require(!auction.ended, "Auction solved.");
+
+        auction.ended = true;
+
+        if (auction.bestAuctionAddress != ownerOf(_id)) {
+            // TODO Transfer
+        }
+
+        // Transférer le NFT au gagnant
+        nftToOwner[_id] = auction.bestAuctionAddress;
+        ownerNftCount[ownerOf(_id)] -= 1;
+        ownerNftCount[auction.bestAuctionAddress] += 1;
+
+        userNfts[auction.bestAuctionAddress].push(_id);
+        _transfer(ownerOf(_id), auction.bestAuctionAddress, _id);
+
+        uint256 index = _indexOf(_id);
+        uint256 i;
+        nftsInAuction[index] = nftsInAuction[nftsInAuction.length - 1];
+        nftsInAuction.pop();
+        nftsAuctionEnded.push(auction);
+
+        for (i = 0; i < auction.auctioners.length; i++) {
+            if (auction.auctioners[i] != auction.bestAuctionAddress) {
+                // TODO Transfer
+            }
+        }
+    }
+
+    function endAuctions() external {
+        Auction storage auction;
+        for(uint256 i = 0; i < nftsInAuction.length; i++){
+            auction = auctions[nftsInAuction[i]];
+            if(auction.auctionEnding <= block.timestamp)endAuction(nftsInAuction[i]);
+        }
+    }
+
+    // Getters
+
+    function getNftToOwner(uint256 _id) external view returns (address) {
+        return nftToOwner[_id];
+    }
+
+    function getOwnerNftCount(address _owner) external view returns (uint256) {
+        return ownerNftCount[_owner];
+    }
+
+    function getUserNfts(
+        address _owner
+    ) external view returns (uint256[] memory) {
+        return userNfts[_owner];
+    }
+
+    function getAuction(uint256 _id) external view returns (Auction memory) {
+        return auctions[_id];
+    }
+
+    function getNftsInAuction() external view returns (uint256[] memory) {
+        return nftsInAuction;
+    }
+
+    function getNftsAuctionEnded() external view returns (Auction[] memory) {
+        return nftsAuctionEnded;
     }
 }
--- contracts/test/NFT.t.sol
+++ contracts/test/NFT.t.sol
@@ -5,18 +5,93 @@ import "forge-std/Test.sol";
 import "../src/NFT.sol";
 
 contract NFTTest is Test {
-    NFT nft;
-
-    event NftAddress(address indexed userAddress);
+    NFT private nft;
+    address private owner = address(0x1);
+    address private user1 = address(0x2);
+    address private user2 = address(0x3);
 
     function setUp() public {
+        // Assign some ETH to the owner and users
+        deal(owner, 10 ether);
+        deal(user1, 10 ether);
+        deal(user2, 10 ether);
+
+        vm.startPrank(owner);
         nft = new NFT();
+        vm.stopPrank();
+    }
+
+    function testMintNFT() public {
+        vm.startPrank(owner);
+        uint256 tokenId = nft.mintNFT(user1, "ipfs://token-uri");
+        assertEq(nft.ownerOf(tokenId), user1);
+        assertEq(nft.tokenURI(tokenId), "ipfs://token-uri");
+        vm.stopPrank();
+    }
+
+    function testStartAuction() public {
+        vm.startPrank(owner);
+        uint256 tokenId = nft.mintNFT(user1, "ipfs://token-uri");
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        nft.startAuction(tokenId, 1 ether, user1);
+        vm.stopPrank();
+
+        NFT.Auction memory auction = nft.getAuction(tokenId);
+        assertEq(auction.id, tokenId);
+        assertEq(auction.ended, false);
+        assertEq(auction.bestAuctionAddress, user1);
+        assertEq(auction.bestAuctionAmount, 1 ether);
+    }
+
+    function testMakeOffer() public {
+        vm.startPrank(owner);
+        uint256 tokenId = nft.mintNFT(user1, "ipfs://token-uri");
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        nft.startAuction(tokenId, 1 ether, user1);
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        nft.makeOffer{value: 2 ether}(tokenId, 2 ether);
+        vm.stopPrank();
+
+        NFT.Auction memory auction = nft.getAuction(tokenId);
+        assertEq(auction.bestAuctionAddress, user2);
+        assertEq(auction.bestAuctionAmount, 2 ether);
+    }
+
+    function testEndAuction() public {
+        vm.startPrank(owner);
+        uint256 tokenId = nft.mintNFT(user1, "ipfs://token-uri");
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        nft.startAuction(tokenId, 1 ether, user1);
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        nft.makeOffer{value: 2 ether}(tokenId, 2 ether);
+        vm.stopPrank();
+
+        // Fast forward time by 1 day
+        vm.warp(block.timestamp + 1 days);
+
+        vm.startPrank(owner);
+        nft.endAuctions();
+        vm.stopPrank();
+
+        NFT.Auction memory auction = nft.getAuction(tokenId);
+        assertEq(auction.ended, true);
+        assertEq(nft.ownerOf(tokenId), user2);
     }
 
     function testStoreUserAddress() public {
         address user1 = address(0x123);
         vm.expectEmit(true, true, true, true);
-        emit NftAddress(user1); // Utiliser l'événement du test
+        // emit NftAddress(user1); // Utiliser l'événement du test
         nft.storeUserAddress(user1);
         assertTrue(nft.isAddressStored(user1));
     }
